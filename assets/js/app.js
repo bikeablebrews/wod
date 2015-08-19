1b95fa57b0bafaacdfee5700e618c1d0baee226b
DT <dmofot@gmail.com>
2015-08-19 13:54:05 -0400

    update terrain tiles

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,7 +19,7 @@ function sidebarClick(id) {
 }
 
 /* Basemap Layers
-   Example basemaps from Stamen/Mapbox */
+   Example basemaps from Mapbox */
 var mapboxTer = L.tileLayer("//{s}.tiles.mapbox.com/v3/matt.72ef5189/{z}/{x}/{y}.png", {
   detectRetina: 'true',
   maxZoom: 21,
